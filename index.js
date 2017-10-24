fb39e147fb2783decaef5726b5c129aa6cb025f8
julianAvaca44 <julian.avaca44@gmail.com>
2017-10-24 01:01:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("pepee");
+alert("pepe");
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+//webpack.config.js
+const path = require('path');
+
+const CONFIG = {
+	"entry": "./index.js",
+	"output": {
+		"path": path.resolve(__dirname, "dist"),
+		"filename": "bundle.js"
+	}
+}
+
+module.exports = CONFIG;
